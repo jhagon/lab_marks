96db5948c818eb30060241a4875445b3a14e5ac9
J.P.Hagon <jphagon@gmail.com>
2013-08-18 12:29:43 +0100

    completed layout and static routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 LabMarks::Application.routes.draw do
+  root  'pages#home'
+
+# match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'pages#help',    via: 'get'
+  match '/about',   to: 'pages#about',   via: 'get'
+  match '/contact', to: 'pages#contact', via: 'get'
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"
