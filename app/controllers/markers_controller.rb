59e4fa4de18e1731e4886e9047086780218e1ad6
J.P.Hagon <jphagon@gmail.com>
2013-08-21 15:43:40 +0100

    added passwords to marker model

--- app/controllers/markers_controller.rb
+++ app/controllers/markers_controller.rb
@@ -13,5 +13,38 @@ class MarkersController < ApplicationController
 
   def new
     @title = "New Marker"
+    @marker = Marker.new
+  end
+
+  def create
+    @marker = Marker.new(params[:marker])
+    if @marker.save
+      # handle a successful save 
+      redirect_to @marker, :notice => "Successfully created marker."
+    else
+      @title = "New Marker"
+      render 'new'
+    end
+  end
+
+  def edit
+    @title = "Edit Marker"
+    @marker = Marker.find(params[:id])
+  end
+
+  def update
+    @marker = Marker.find(params[:id])
+    if @marker.update_attributes(params[:marker])
+      redirect_to @marker, :notice  => "Successfully updated marker."
+    else
+      render :action => 'edit'
+    end
+  end
+
+
+  def destroy
+    @marker = Marker.find(params[:id])
+    @marker.destroy
+    redirect_to markers_url, :notice => "Successfully destroyed marker."
   end
 end
